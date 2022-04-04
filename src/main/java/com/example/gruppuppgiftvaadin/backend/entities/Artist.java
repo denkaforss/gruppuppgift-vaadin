dfb4469ee8c8edf45dd1c92d76a4c77f16a88f11
denkaforss <dennis.forss@gmail.com>
2022-04-04 15:06:57 +0200

    created entities
    songs, album and artist

--- src/main/java/com/example/gruppuppgiftvaadin/backend/entities/Artist.java
+++ src/main/java/com/example/gruppuppgiftvaadin/backend/entities/Artist.java
@@ -1,7 +1,57 @@
 package com.example.gruppuppgiftvaadin.backend.entities;
 
-import javax.persistence.Entity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.util.Set;
 
 @Entity
 public class Artist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    private int id;
+
+    @Column
+    @Getter
+    @Setter
+    @NotNull
+    private String artistName;
+
+    @Column
+    @Getter
+    @Setter
+    private LocalDate startingYear;
+
+    @Column
+    @Getter
+    @Setter
+    private String homeCountry;
+
+    @OneToMany(mappedBy = "artist")
+    @JsonIgnore
+    @Getter
+    @Setter
+    public Set<Songs> songsSet;
+
+    @OneToMany(mappedBy = "artist")
+    @JsonIgnore
+    @Getter
+    @Setter
+    public Set<Album> albumSet;
+
+    public Artist(){}
+
+    public Artist(String artistName, LocalDate startingYear, String homeCountry, Set<Songs> songsSet, Set<Album> albumSet) {
+        this.artistName = artistName;
+        this.startingYear = startingYear;
+        this.homeCountry = homeCountry;
+        this.songsSet = songsSet;
+        this.albumSet = albumSet;
+    }
 }
--- src/main/java/com/example/gruppuppgiftvaadin/backend/entities/Songs.java
+++ src/main/java/com/example/gruppuppgiftvaadin/backend/entities/Songs.java
@@ -3,10 +3,8 @@ package com.example.gruppuppgiftvaadin.backend.entities;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 public class Songs {
@@ -14,5 +12,37 @@ public class Songs {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Getter
+    private int id;
+
+    @Column
+    @NotBlank
+    @Getter
     @Setter
+    private String songName;
+
+    @Column
+    @Getter
+    @Setter
+    private int songLength;
+
+    @ManyToOne
+    @JoinColumn(name = "artist_id")
+    @Getter
+    @Setter
+    private Artist artist;
+
+    @ManyToOne
+    @JoinColumn(name = "album_id")
+    @Getter
+    @Setter
+    private Album album;
+
+    public Songs(){}
+
+    public Songs(String songName, int songLength, Artist artist, Album album) {
+        this.songName = songName;
+        this.songLength = songLength;
+        this.artist = artist;
+        this.album = album;
+    }
 }
