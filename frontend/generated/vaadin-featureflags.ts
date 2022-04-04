0d2c671725d94b6d1f0fb1b5e74cd69a3c69a064
DouglasAlexa <92862853+DouglasAlexa@users.noreply.github.com>
2022-04-04 16:05:04 +0200

    Test

--- frontend/generated/vaadin-featureflags.ts
+++ frontend/generated/vaadin-featureflags.ts
@@ -2,5 +2,9 @@
 window.Vaadin = window.Vaadin || {};
 window.Vaadin.featureFlags = window.Vaadin.featureFlags || {};
 window.Vaadin.featureFlags.exampleFeatureFlag = false;
+<<<<<<< Updated upstream
 window.Vaadin.featureFlags.viteForFrontendBuild = false;
+=======
+window.Vaadin.featureFlags.viteForFrontendBuild = true;
+>>>>>>> Stashed changes
 window.Vaadin.featureFlags.mapComponent = false;
--- src/main/java/com/example/gruppuppgiftvaadin/frontend/views/MainView.java
+++ src/main/java/com/example/gruppuppgiftvaadin/frontend/views/MainView.java
@@ -1,4 +1,32 @@
 package com.example.gruppuppgiftvaadin.frontend.views;
 
-public class MainView {
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.component.tabs.Tabs;
+import com.vaadin.flow.router.Route;
+
+@Route("/")
+public class MainView extends AppLayout {
+
+    public MainView() {
+        DrawerToggle toggle = new DrawerToggle();
+
+        H1 title = new H1("MythicTunes");
+        title.getStyle()
+                .set("font-size", "var(--lumo-font-size-1")
+                .set("margin", "0");
+
+        Tab landingPage = new Tab("Landing Page");
+        Tab gridView = new Tab("Grid view");
+
+        Tabs tabs = new Tabs();
+        tabs.add(landingPage, gridView);
+
+        addToDrawer(tabs);
+        addToNavbar(toggle, title);
+    }
+
+
 }
