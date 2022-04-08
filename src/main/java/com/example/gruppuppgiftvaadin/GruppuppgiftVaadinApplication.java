3ea2631de0b727fe12747d2826931d57222f6335
denkaforss <dennis.forss@gmail.com>
2022-04-08 15:04:33 +0200

    moved stuff around

--- src/main/java/com/example/gruppuppgiftvaadin/GruppuppgiftVaadinApplication.java
+++ src/main/java/com/example/gruppuppgiftvaadin/GruppuppgiftVaadinApplication.java
@@ -35,7 +35,28 @@ public class GruppuppgiftVaadinApplication implements AppShellConfigurator {
 
             Artist avantasia = new Artist("Avantasia", LocalDate.of(1999, 4, 14), "Germany", appUser, "Avantasia är ett power metalprojekt skapat av Edguys sångare Tobias Sammet. Hittills har sju skivor givits ut varav fem är album The Metal Opera Part I, The Metal Opera Part II, Lost In Space part I EP, Lost In Space Part II EP");
 
+            Artist artist2 = new Artist("Miss May I", LocalDate.of(2001, 1, 1), "denmark", appUser, "Miss May I is an American metalcore band from Troy, Ohio. Formed in 2007,[3] they signed to Rise Records in 2008 and released their debut album, Apologies Are for the Weak through the label while the members were still attending high school.");
+
+            Artist artist4 = new Artist("Bullet For My Valentine", LocalDate.of(2001, 1, 1), "Spain", appUser, "Bullet for My Valentine är ett walesiskt heavy metalband från Bridgend, bildade år 1998. Bandet består av Matthew Tuck (sång, kompgitarr), Michael Paget (sologitarr, bakgrundssång), Michael Thomas (trummor) och Jamie Mathias (bas).");
+
+
             Album album = new Album("The Wicked Symphony", LocalDate.of(2010, 4, 3), avantasia, appUser, "The Wicked Symphony är det fjärde power metalalbumet skapat av Avantasia, ett av Tobias Sammets projekt. På skivan medverkar flertalet musiker från diverse olika band, exempelvis Tim \"Ripper\" Owens som tidigare sjöng i Judas Priest och Iced Earth.", "/images/avantasia.jpg");
+
+            Album album666 = new Album("test album", LocalDate.of(1994, 5, 12), artist2, appUser, "bla bla bla", "bild osv");
+
+            Album album2 = new Album("Shadows Inside", LocalDate.of(2002, 2, 2), artist2, appUser, "Shadows Inside is the sixth studio album from American metalcore band Miss May I. Released on 2 June 2017, this was the band's first release on SharpTone Records after leaving Rise Records.[1]", "/images/shadowsinside.jpg");
+
+            Album album3 = new Album("Moonglow", LocalDate.of(2019, 2, 2), avantasia, appUser, "Moonglow is the eighth full-length album by Tobias Sammet's German metal opera project Avantasia. It was released on 15 February 2019 through Nuclear Blast.", "/images/moonglow.png");
+
+            Album album4 = new Album("Fever", LocalDate.of(2010, 2, 2), artist4, appUser, "Bandets fjärde studioalbum, Fever, utgavs den 27 april 2010 och nådde plats nummer 3 på Billboard 200, vilket gjorde det till bandets bäst placerade album på Billboards topplista; detta personbästa står sig än i dag (2015).", "/images/fever.jpg");
+
+            Songs songs2 = new Songs("Shadows Inside", 236, artist2, album2, appUser);
+
+            Songs songs3 = new Songs("Third song", 149, avantasia, album3, appUser);
+            
+            Songs songs4 = new Songs("Fourth song", 149, artist4, album4, appUser);
+
+
 
             Songs avantSongs1 = new Songs("The Wicked Symphony", 568, avantasia, album, appUser);
             Songs avantSongs2 = new Songs("Wastelands", 284, avantasia, album, appUser);
@@ -49,24 +70,6 @@ public class GruppuppgiftVaadinApplication implements AppShellConfigurator {
             Songs avantSongs10 = new Songs("States of Matter", 237, avantasia, album, appUser);
             Songs avantSongs11 = new Songs("The Edge", 252, avantasia, album, appUser);
 
-
-            Artist artist2 = new Artist("Miss May I", LocalDate.of(2001, 1, 1), "denmark", appUser, "Miss May I is an American metalcore band from Troy, Ohio. Formed in 2007,[3] they signed to Rise Records in 2008 and released their debut album, Apologies Are for the Weak through the label while the members were still attending high school.");
-
-            Album album666 = new Album("test album", LocalDate.of(1994, 5, 12), artist2, appUser, "bla bla bla", "bild osv");
-
-            Album album2 = new Album("Shadows Inside", LocalDate.of(2002, 2, 2), artist2, appUser, "Shadows Inside is the sixth studio album from American metalcore band Miss May I. Released on 2 June 2017, this was the band's first release on SharpTone Records after leaving Rise Records.[1]", "/images/shadowsinside.jpg");
-
-            Songs songs2 = new Songs("Shadows Inside", 236, artist2, album2, appUser);
-
-            Album album3 = new Album("Moonglow", LocalDate.of(2019, 2, 2), avantasia, appUser, "Moonglow is the eighth full-length album by Tobias Sammet's German metal opera project Avantasia. It was released on 15 February 2019 through Nuclear Blast.", "/images/moonglow.png");
-
-            Songs songs3 = new Songs("Third song", 149, avantasia, album3, appUser);
-
-            Artist artist4 = new Artist("Bullet For My Valentine", LocalDate.of(2001, 1, 1), "Spain", appUser, "Bullet for My Valentine är ett walesiskt heavy metalband från Bridgend, bildade år 1998. Bandet består av Matthew Tuck (sång, kompgitarr), Michael Paget (sologitarr, bakgrundssång), Michael Thomas (trummor) och Jamie Mathias (bas).");
-            
-            Album album4 = new Album("Fever", LocalDate.of(2010, 2, 2), artist4, appUser, "Bandets fjärde studioalbum, Fever, utgavs den 27 april 2010 och nådde plats nummer 3 på Billboard 200, vilket gjorde det till bandets bäst placerade album på Billboards topplista; detta personbästa står sig än i dag (2015).", "/images/fever.jpg");
-
-            Songs songs4 = new Songs("Fourth song", 149, artist4, album4, appUser);
 
             artistRepo.saveAll(List.of(avantasia, artist2, artist4));
             albumRepo.saveAll(List.of(album, album2, album3, album4, album666));
