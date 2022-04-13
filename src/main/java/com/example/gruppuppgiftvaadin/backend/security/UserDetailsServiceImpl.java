b539072b251b95fd9cf1615ff2957ecc6e5a6cfd
denkaforss <dennis.forss@gmail.com>
2022-04-13 13:38:20 +0200

    added admin role

--- src/main/java/com/example/gruppuppgiftvaadin/backend/security/UserDetailsServiceImpl.java
+++ src/main/java/com/example/gruppuppgiftvaadin/backend/security/UserDetailsServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.gruppuppgiftvaadin.backend.security;
 import com.example.gruppuppgiftvaadin.backend.entities.AppUser;
 import com.example.gruppuppgiftvaadin.backend.repositories.AppUserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -10,6 +11,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
@@ -21,6 +23,10 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
         AppUser appUser = appUserRepo.findByUsername(username).orElseThrow();
+
+        if (Objects.equals(appUser.getUsername(), "admin")) {
+            return new User(appUser.getUsername(), appUser.getPassword(), List.of(new SimpleGrantedAuthority("ROLE_ADMIN")));
+        }
 
         return new User(appUser.getUsername(), appUser.getPassword(), List.of());
     }
